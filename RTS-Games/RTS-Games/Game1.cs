b21576abe3f8fda5f8d53a4902dcafa8a2958971
Marius <Zecon_asdjak@live.dk>
2020-02-14 11:21:06 +0100

    Auto stash before merge of "Marius" and "origin/Marius"

--- RTS-Games/RTS-Games/Game1.cs
+++ RTS-Games/RTS-Games/Game1.cs
@@ -18,8 +18,6 @@ namespace RTS_Games
             Content.RootDirectory = "Content";
         }
 
-        
-
         /// <summary>
         /// Allows the game to perform any initialization it needs to before starting to run.
         /// This is where it can query for any required services and load any non-graphic
