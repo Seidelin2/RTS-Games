80e231a39162af982d1c6d4ff0706f1b125891a0
Casper <Seidelin2@hotmail.com>
2020-02-18 09:44:32 +0100

    Goldmine Semaphore

--- RTS-Games/RTS-Games/Worker.cs
+++ RTS-Games/RTS-Games/Worker.cs
@@ -2,12 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace RTS_Games
 {
     class Worker
     {
+        MouseState mouseState = Mouse.GetState();
 
+        
+
+        
     }
 }
